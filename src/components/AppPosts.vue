88e0dc7fb349cbcb5718492634a75d93a2da49e8
Arimos <amilinkovic.ns@gmail.com>
2018-08-16 20:53:03 +0200

    zadatak 3

--- src/components/AppPosts.vue
+++ src/components/AppPosts.vue
@@ -1,11 +1,39 @@
 <template>
   <div >
-      <p>Posts</p>
+      <table>
+        <ul v-for="post in posts" :key="post.id" class="list-group">
+            <li class="list-group-item">Created at : {{post.createdAt}} Post title: {{post.title}}</li><hr>
+            <h4>Blog Post</h4>
+            <li class="list-group-item list-group-item-dark">{{post.text}}</li>
+        </ul>
+      </table>
   </div>
 </template>
 
 <script>
+
+import { posts } from '../services/Posts.js'
+
 export default {
   
+  data()
+  { 
+      name:'AppPosts'
+      return {
+          posts: [],
+          
+      }
+  },
+
+  beforeRouteEnter(to, from, next) {
+    posts.getAll()
+      .then((response) => {
+          next((vm) => {
+            vm.posts = response.data
+          })
+      })
+  }
   
 }
+
+</script>
